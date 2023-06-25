3a32c4896a6cf27637a1b54f34a39029b12abb1d
angelip2303 <angel.iglesias.prestamo@gmail.com>
2023-06-25 10:32:10 +0000

    improving the id

--- wikidata-rs/src/id.rs
+++ wikidata-rs/src/id.rs
@@ -68,12 +68,12 @@ impl<'a> From<&'a str> for Id {
 impl From<Id> for u32 {
     fn from(id: Id) -> Self {
         match id {
-            Id::Fid(fid) => u32::from(Id::Lid(fid.0)) + (fid.1 as u32 * 3_000_000_000),
+            Id::Fid(fid) => u32::from(Id::Lid(fid.0)) + (fid.1 as u32 + 3_000_000_000),
             Id::Lid(lid) => lid.0 as u32 + 2_000_000_000,
             Id::Pid(pid) => pid.0 as u32 + 1_000_000_000,
             Id::Qid(qid) => qid.0 as u32,
             Id::Sid(sid) => {
-                u32::from(Id::Lid(sid.0)) + (sid.1 as u32 * 3_000_000_000) + 500_000_000
+                u32::from(Id::Lid(sid.0)) + (sid.1 as u32 + 3_000_000_000) + 500_000_000
             }
             Id::DataType(dt) => u8::from(&dt) as u32 + 4_000_000_000,
         }
